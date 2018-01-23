5de55002194a5d3e0e259a3d10250bfb507ca75e
Daniel Luxemburg <daniel.luxemburg@gmail.com>
2018-01-23 13:10:24 -0500

    mute

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ class NoSleep {
       // Set up no sleep video element
       this.noSleepVideo = document.createElement('video')
 
+      this.noSleepVideo.setAttribute('muted', '')
       this.noSleepVideo.setAttribute('playsinline', '')
       this.noSleepVideo.setAttribute('src', mediaFile)
 
